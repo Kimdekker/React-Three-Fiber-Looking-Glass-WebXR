e369c7aeca9a8e7cd908f4e9e6d35423be6c3d2d
Kim Dekker <kim11dekker@gmail.com>
2024-12-13 11:26:43 +0100

    error fix

--- src/App.js
+++ src/App.js
@@ -1,48 +1,57 @@
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { VRCanvas } from '@react-three/xr'
-import { LookingGlassWebXRPolyfill, LookingGlassConfig } from '@lookingglass/webxr'
+import { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { XR, VRButton } from '@react-three/xr';
+import { LookingGlassWebXRPolyfill, LookingGlassConfig } from '@lookingglass/webxr';
 
-const config = LookingGlassConfig
-config.tileHeight = 512
-config.numViews = 45
-config.targetY = 0
-config.targetZ = 0
-config.targetDiam = 3
-config.fovy = (14 * Math.PI) / 180
-new LookingGlassWebXRPolyfill()
+// Configure Looking Glass settings
+const config = LookingGlassConfig;
+config.tileHeight = 512;
+config.numViews = 45;
+config.targetY = 0;
+config.targetZ = 0;
+config.targetDiam = 3;
+config.fovy = (14 * Math.PI) / 180;
+
+// Initialize Looking Glass WebXR Polyfill
+new LookingGlassWebXRPolyfill();
 
 function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
+  const ref = useRef();
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+
+  // Rotate the box every frame
+  useFrame((state, delta) => (ref.current.rotation.x += delta));
+
   return (
     <mesh
       {...props}
       ref={ref}
       scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
-  )
+  );
 }
 
 export default function App() {
   return (
-    <VRCanvas>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </VRCanvas>
-  )
+    <>
+      {/* Add the VR Button to the DOM */}
+      <VRButton />
+      <Canvas>
+        <XR>
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </XR>
+      </Canvas>
+    </>
+  );
 }
